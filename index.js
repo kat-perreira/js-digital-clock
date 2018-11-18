1e755387a80c36b209bd981d59e374b9b9db8ef2
Kat Perreira <kat.perreira@gmail.com>
2018-11-17 22:35:42 -0800

    added in some animation to make the colors change

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+
+let d,h,m,s,animate;
+
+function init(){
+  d=new Date();
+  h=d.getHours();
+  m=d.getMinutes();
+  s=d.getSeconds();
+  clock();
+}
+
+function clock(){
+  s += 1;
+  if(s == 60){
+    s = 0;
+    m += 1;
+    if(m == 60){
+      m = 0;
+      h += 1;
+      if(h == 24){
+        h = 0;
+      }
+    }
+
+    h = ((h + 11) % 12 + 1)
+  }
+  // h = ((h + 11) % 12 + 1)
+
+    $('sec',s);
+    $('min',m);
+    $('hr',h);
+    animate=setTimeout(clock,1000);
+  }
+
+  function $(id,val){
+    if(val<10){
+      val='0'+val;
+    }
+    document.getElementById(id).innerHTML=val;
+
+  }
+
+  window.onload=init;
